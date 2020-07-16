96397ff063468cd37ed07e5423521d97ed499171
DarekR88 <dradmclad@gmail.com>
2020-07-16 13:02:53 -0500

    StyleSheet

---
+++ App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.textInput} />
+        <Button title="ADD"/>
+      </View>
+      <View>
+
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row", 
+    justifyContent: "space-between", 
+    alignItems: "center"
+  },
+  textInput: {
+    width: "80%", 
+    borderColor: "black", 
+    borderWidth: 1, 
+    padding: 10
+  },
+});
+
