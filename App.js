9015e94bddadc4239e95a1ae9278185767ebf304
DarekR88 <dradmclad@gmail.com>
2020-07-16 14:08:45 -0500

    ScrollView

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGaol] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGaol(enteredText);
+  };
+
+  const addGoalHnadler = () => {
+    setGoals((goalsList) => [...goalsList, enteredGoal]);
+  };
 
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.textInput} />
-        <Button title="ADD"/>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHnadler} />
       </View>
-      <View>
-
-      </View>
+      <ScrollView>
+        {goals.map((goal) => (
+          <View key={goal} style={styles.goals}>
+            <Text >{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,18 +39,24 @@ export default function App() {
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50
+    padding: 50,
   },
   inputContainer: {
-    flexDirection: "row", 
-    justifyContent: "space-between", 
-    alignItems: "center"
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   textInput: {
-    width: "80%", 
-    borderColor: "black", 
-    borderWidth: 1, 
-    padding: 10
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
   },
+  goals: {
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
-
